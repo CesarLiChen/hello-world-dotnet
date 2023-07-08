21ee18513575016798d536f4c461badac32054ea
Cesar Li (WSL) <cesarli619@gmail.com>
2023-07-07 20:41:59 -0400

    Enhances app, adds README

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, WORLD");
+            Console.WriteLine("What is your name");
+
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+
+            // {Environment.NewLine} is a platform and language independent line break.
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.WriteLine("\n");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# A Hellow World application in .NET
+
+- `dotnet new  console --framework net7.0` *creates project template that display Hello World*
+- `dotnet run` *to run*
